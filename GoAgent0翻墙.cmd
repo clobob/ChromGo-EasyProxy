af99fee687ad96414609438746fa3df51c1e7bd4
douliyou <douliyou@live.cn>
2018-01-10 13:29:41 +0800

    Initial commit

---
+++ GoAgent0翻墙.cmd
@@ -0,0 +1,9 @@
+%%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a 
+cls
+@echo off
+start "" "%~dp0Agent0\goproxy.exe"
+IF EXIST %~dp0Browser\chrome.exe (
+    start %~dp0Browser\chrome.exe --user-data-dir=%~dp0chrome-user-data --proxy-pac-url=%~dp0go.pac https://www.bannedbook.org/bnews/fq/?utm_source=chgo-0
+) ELSE (
+    start chrome.exe --user-data-dir=%~dp0chrome-user-data --proxy-pac-url=%~dp0go.pac https://www.bannedbook.org/bnews/fq/?utm_source=chgo-0
+)
