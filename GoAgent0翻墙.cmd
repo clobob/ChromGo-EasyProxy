b03d628430d5e47e0dc2bf09d313035cc431995c
Dou Liyou <douliyou@live.cn>
2018-06-11 10:04:31 +0800

    更改启动主页为Google

--- GoAgent0翻墙.cmd
+++ GoAgent0翻墙.cmd
@@ -3,7 +3,7 @@ cls
 @echo off
 start "" "%~dp0Agent0\goproxy.exe"
 IF EXIST %~dp0Browser\chrome.exe (
-    start %~dp0Browser\chrome.exe --user-data-dir=%~dp0chrome-user-data --proxy-pac-url=%~dp0go.pac https://www.bannedbook.org/bnews/fq/?utm_source=chgo-0
+    start %~dp0Browser\chrome.exe --user-data-dir=%~dp0chrome-user-data --proxy-pac-url=%~dp0go.pac https://www.google.com
 ) ELSE (
-    start chrome.exe --user-data-dir=%~dp0chrome-user-data --proxy-pac-url=%~dp0go.pac https://www.bannedbook.org/bnews/fq/?utm_source=chgo-0
+    start chrome.exe --user-data-dir=%~dp0chrome-user-data --proxy-pac-url=%~dp0go.pac https://www.google.com
 )
